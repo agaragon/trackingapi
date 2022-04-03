bc0ba5b772acaf0955fd820cb6a3d611520ad888
Andre Argon <andregaragon@gmail.com>
2022-04-03 20:07:20 -0300

    parametros faltantes do acesso adicionados

--- models/Access.go
+++ models/Access.go
@@ -4,5 +4,7 @@ type Access {
 	Ip string `json:"ip"`
 	Ua string `json:"up"`
 	Utm string `json:"utm"`
+	Url string `json:"url"`
+	SessionId string `json:"sessionId"`
 	ClientId string `json:"clientId"`
 }
